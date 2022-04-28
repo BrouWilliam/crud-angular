9d2e11a8fab81ec4966e175fc70eaf30ac6ffbd3
brouwilliamog <williamdzzn@hotmail.com>
2022-04-28 08:39:42 -0300

    Initialize project and create function new element

---
+++ src/app/shared/element-dialog/element-dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { PeriodicElement } from 'src/app/views/home/home.component';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-element-dialog',
+  templateUrl: './element-dialog.component.html',
+  styleUrls: ['./element-dialog.component.scss']
+})
+export class ElementDialogComponent implements OnInit {
+
+  element!: PeriodicElement
+
+  constructor(
+    public dialogRef: MatDialogRef<ElementDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: PeriodicElement,) { }
+
+  ngOnInit(): void {
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+}
